68575136280cf15d6842dc53934506714f8fa3ee
KIMYUNMEE <jayoo8665@naver.com>
2022-04-18 23:13:01 +0900

    React공부 - state를 이용해서 버튼을 누르면 숫자 올라가기 및 블로그 수정버튼 만들기

--- src/App.js
+++ src/App.js
@@ -5,13 +5,35 @@ let posts = "강남 고기 맛집";
 let colors = { color: 'bule', fontSize: '30px' };
 function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집','윤미최고']);
+  let [따봉, 따봉변경] = useState(0);
+  function 감자() {
+    let newArray = [...글제목];
+    newArray[0] = "여자코트 추천";
+    글제목변경(newArray);
+  }
+  //글제목함수의 가나다순 정렬
+  //  function 감자() {
+  //   let 고구마 = [...글제목];
+  //   고구마.sort();
+  //   글제목변경(고구마);
+  // }
+
+//1.글제목이라는 state는 직접 수정할 수 없기 때문에 복사본을 만듭니다. 
+//2.글제목이라는 state의 복사본을 만들어 newArray라는 변수에 저장합니다. 
+//3.newArray의 0번째 데이터를 '여자코트 추천'으로 변경합니다.
+//4.그리고 newArray를 글제목변경() 함수 안에 넣어서 글제목 state를 변경합니다. 
+//이렇게 했을 시 문제가 생기는데 1번에서 문제가 생겼습니다.
+//원래 자바스크립트 내에서 array나 object 자료형은 = 등호로 복사하시면 각각 별개의 자료형이 생성되는게 아니라 값을 공유합니다. 그렇기 때문에 복사본에 값이 변경되면 원본도 같이 변경됩니다.
+//그렇기 때문에 단독복사본을 만들기 위해서 deep copy인 ...(=spread연산자)를 이용합니다. 이러면 값공유가 되지 않고 서로 독립적인 값을 가지게 됩니다.
+//...란spread 연산자라고하는 ES6 신문법입니다. array나 object 자료형 왼쪽에 붙일 수 있으며 뜻은 중괄호나 대괄호를 벗겨주세요 라는 뜻입니다. 
   return (
     <div className="App">
       <div className="black-nav">
         <div className={colors}>개발 BLOG</div>
       </div>
       <div className="list">
-        <h3>{글제목[0]}</h3>
+        <button onClick={감자}>욤</button>
+        <h3>{글제목[0]}<span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span>{따봉}</h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
@@ -41,3 +63,6 @@ export default App;
 //2))state
 //1.데이터 보관은 변수에 저장할 수 도 있지만 state에 저장할 수 있습니다.
 //2.state는 state의 값이 변경되면 새로고침 없이 html이 자동으로 재렌더링 됩니다!! 반면 변수는 값이 바뀌어도 자동으로 재렌더링, 즉 update가 되지 않습니다.
+//3.React jsx에서는 html에서 onclick를 다르게 쓰는데 1. Click이 대문자 2.{} 중괄호 사용 3.중괄호 안에 코드가 아닌 함수를 적기
+//3. 단 es6문법인 ()=>{} 화살표 함수도 사용 가능합니다. 여기에서 {}에는 일반코드도 넣을 수 있습니다.
+//4. state는 변수와는 다르게 값을 변경하실 때 지정된 변경함수를 쓰셔야합니다. => 따봉변경( 대체할 데이터 )
